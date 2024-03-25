d46eeb87c0633845517ffeac3cb6d2edaee8d7d9
ChrisferDev <ferchris82@hotmail.com>
2024-03-25 16:46:44 -0500

    Versión 1.1

---
+++ src/main/java/com/chrisferdev/tareas/controller/indexControlador.java
@@ -0,0 +1,16 @@
+package com.chrisferdev.tareas.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class indexControlador implements Initializable{
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
