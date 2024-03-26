b7c143df6822130bd71629a7bb938dddb8e9539a
ChrisferDev <ferchris82@hotmail.com>
2024-03-25 19:12:44 -0500

    Versión 1.2

--- src/main/java/com/chrisferdev/tareas/TareasApplication.java
+++ src/main/java/com/chrisferdev/tareas/TareasApplication.java
@@ -3,11 +3,17 @@ package com.chrisferdev.tareas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.chrisferdev.tareas.view.SistemasTareasFx;
+
+import javafx.application.Application;
+
 @SpringBootApplication
 public class TareasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TareasApplication.class, args);
+		//SpringApplication.run(TareasApplication.class, args);
+		Application.launch(SistemasTareasFx.class, args);
 	}
 
 }
+
